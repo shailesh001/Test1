6e4355da834de97fdf25673d8b878000b4962c09
Shailesh Patel <shailesh001@mac.com>
2020-08-10 19:40:04 +0100

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Shailesh Patel on 10/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
